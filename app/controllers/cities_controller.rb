1bfccb651c51ef99519031d5bf41e48bee37dcc1
李阳 <admin@admindeMacBook-Air.local>
2017-08-30 00:39:52 +0800

    2-5 更新城市天气

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,6 +2,19 @@ class CitiesController < ApplicationController
 
   def index
     @cities = City.all
+  end
+
+  def update_temp
+    city = City.find(params[:id])
+
+    response = RestClient.get "http://v.juhe.cn/weather/index",
+                              :params => { :cityname => city.juhe_id, :key => "fb573b4410a16c4e30976cc1515ea19a"}
+    data = JSON.parse(response.body)
+
+    city.update( :current_temp => data["result"]["sk"]["temp"])
+
+    redirect_to cities_path
+
 
   end
 end
