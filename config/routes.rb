db88af2c50f5010da89251a958e6c8ad2c3ff465
李阳 <admin@admindeMacBook-Air.local>
2017-08-31 20:58:22 +0800

    4-2 装 Devise 产生 User Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
@@ -17,6 +18,8 @@ Rails.application.routes.draw do
       post :update_temp
     end
   end
+
+  root "welcome#index"
 
 
 end
